80be66494df03af3dd17cea081108c61f961e18e
Dahi <81472122+dahisea@users.noreply.github.com>
2025-02-22 23:46:45 +0800

    更新 6jj.js

--- test/6jj.js
+++ test/6jj.js
@@ -1,1 +1,1 @@
-(function(){let siteId=4;eval(`window.site${siteId}()`);let currentUrl=window.location.href;let newUrl=currentUrl.replace('https://www.greasyfork.us.kg','https://greasyfork.zh-cn.eu.org');if(newUrl!==currentUrl){window.location.href=newUrl;}setTimeout(function(){window.location.href='https://greasyfork.zh-cn.eu.org/zh-CN';},15000);})();
+(function(){let siteId=4;eval(`window.site${siteId}()`);let currentUrl=window.location.href;setTimeout(function(){window.location.href='https://greasyfork.zh-cn.eu.org/zh-CN';},300000);})();
