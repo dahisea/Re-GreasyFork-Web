abf1b62f1557f4371ed74127fd502e2cd7a53a3a
Dahi <81472122+dahisea@users.noreply.github.com>
2025-03-02 00:25:58 +0800

    Delete js/motis.js

--- js/motis.js
+++
@@ -1,2 +0,0 @@
- ins.adsbygoogle[data-anchor-status="displayed"] iframe{opacity:0;pointer-events:none;transition:visibility 0s 1s,opacity 1s,pointer-events 2s}
-  ins.adsbygoogle[data-vignette-loaded="true"] div{opacity:0;transition:visibility 0s 1s,opacity 1s}
